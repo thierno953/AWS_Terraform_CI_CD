b44e4aa576fe66d642a53b3c643232c8ea9abc06
thierno <77080504+thierno953@users.noreply.github.com>
2023-07-27 22:56:17 +0200

    added VPC and GH Actions

--- src/main.tf
+++ src/main.tf
@@ -27,12 +27,12 @@ module "tf-state" {
   bucket_name = "cc-tf-state-backend-ci-cd"
 }
 
-# module "vpc-infra" {
-#   source = "./modules/vpc"
+module "vpc-infra" {
+  source = "./modules/vpc"
 
-#   # VPC Input Vars
-#   vpc_cidr             = local.vpc_cidr
-#   availability_zones   = local.availability_zones
-#   public_subnet_cidrs  = local.public_subnet_cidrs
-#   private_subnet_cidrs = local.private_subnet_cidrs
-# }
+  # VPC Input Vars
+  vpc_cidr             = local.vpc_cidr
+  availability_zones   = local.availability_zones
+  public_subnet_cidrs  = local.public_subnet_cidrs
+  private_subnet_cidrs = local.private_subnet_cidrs
+}
